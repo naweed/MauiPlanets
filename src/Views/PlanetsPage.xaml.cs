e9e46ccca951d620cbefc32e1364a7a0f6a6d522
Naweed Akram <naweed@xgeno.com>
2022-08-19 18:03:12 +0300

    Added Side Menu while clicking on Profile Pic

--- src/Views/PlanetsPage.xaml.cs
+++ src/Views/PlanetsPage.xaml.cs
@@ -2,7 +2,9 @@
 
 public partial class PlanetsPage : ContentPage
 {
-	public PlanetsPage()
+    private const uint AnimationDuration = 800u;
+
+    public PlanetsPage()
 	{
 		InitializeComponent();
 	}
@@ -18,5 +20,26 @@ public partial class PlanetsPage : ContentPage
     async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
     {
         await Navigation.PushAsync(new PlanetDetailsPage(e.CurrentSelection.First() as Planet));
+    }
+
+    async void ProfilePic_Clicked(System.Object sender, System.EventArgs e)
+    {
+        // Reveal our menu and move the main content out of the view
+        _ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+        await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+        _ = MainContentGrid.FadeTo(0.8, AnimationDuration);
+    }
+
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+        await CloseMenu();
+    }
+
+    private async Task CloseMenu()
+    {
+        //Close the menu and bring back back the main content
+        _ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+        await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
     }
 }
